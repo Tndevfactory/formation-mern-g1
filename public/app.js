9f714fb40e36d4d3a845eea1053bb929b7b47af5
ch <tndevfactory@gmail.com>
2023-11-19 18:26:06 +0100

    js submit form

--- public/app.js
+++ public/app.js
@@ -1,33 +1,21 @@
+// collecte des information de la page html
 const form = document.querySelector("#form");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const emailError = document.querySelector("#emailError");
 const passwordError = document.querySelector("#passwordError");
 
-if (form) {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const email = e.target[0].value;
-    const password = e.target[1].value;
+  const email = e.target[0].value;
+  const password = e.target[1].value;
 
-    if (email == "") {
-      emailError.innerHTML = "inserer un email svp";
-    }
+  if (email === "") {
+    emailError.innerHTML = "Veuillez entrez l'adresse mail";
+  }
 
-    if (password == "") {
-      passwordError.innerHTML = "inserer un mot de passe svp";
-    }
-
-    if (email !== "" || password !== "") {
-      location.href = "http://127.0.0.1:5500/dashboard.html";
-    }
-  });
-
-  email.addEventListener("input", (e) => {
-    emailError.innerHTML = "";
-  });
-  password.addEventListener("input", (e) => {
-    passwordError.innerHTML = "";
-  });
-}
+  if (password === "") {
+    passwordError.innerHTML = "Veuillez entrez le mot de passe";
+  }
+});
